5584f173178a4a35be478c54d1b3f8b41e1b758c
blainelawson <blaine.lawson@gmail.com>
2020-01-14 17:47:08 -0500

    moved Dataset generation code to own class DatasetGenerator

--- bin/buy_low_sell_high.rb
+++ bin/buy_low_sell_high.rb
@@ -4,7 +4,7 @@ file_path = '../db/input/currency_sample.in.txt'
 file = File.open(file_path)
 file_data = File.read(file_path).split("\r\n")
 
-Dataset.generate(file_data)
+DatasetGenerator.generate(file_data)
 
 Dataset.display
 
