2fa1ae11e3936e46f68e980dcda74363e5dea522
blainelawson <blaine.lawson@gmail.com>
2020-01-14 17:49:36 -0500

    Merged create_dataset_generator_class into master

---
+++ lib/dataset_generator.rb
@@ -0,0 +1,42 @@
+class DatasetGenerator
+    def self.generate(file_data)
+        datasets_amt = file_data[0].to_i # Indicates number of sets (useable for tests)
+
+        # Initialize dataset start point
+        start_index = 1
+        end_index = file_data[1].split(" ").last.to_i + 2 # +1 for currency multiplier row and initial set number row
+
+        # Find first dataset's denoms and prices--this is repeatable and also lets me know how many 
+        # rows of data are in each set--what index to start the process on again.
+        datasets_amt.times do
+            @item = Dataset.new
+
+            denoms = file_data[start_index].split(" ").first.to_i
+            prices = file_data[start_index].split(" ").last.to_i
+
+            currency_multipliers = file_data[start_index + 1].split(" ").map {|e| e.to_i}
+            row_end_price = 0
+            i=0
+
+            (prices).times do 
+                row = file_data[start_index + 2 + i].split(" ").map {|e| e.to_i}
+
+                #convert prices 
+                row_end_price = row.map.with_index do |currency, index|
+                    if index >= currency_multipliers.length
+                        currency
+                    else
+                        currency * currency_multipliers[index..currency_multipliers.length].reduce(:*).to_i
+                    end
+                end
+
+                @item.prices << row_end_price
+                i = i + 1
+            end
+
+            # advance start index to next dataset (2 rows of qualifying data followed by rows of prices)
+            start_index += 2 + prices
+        end
+    end
+
+end
