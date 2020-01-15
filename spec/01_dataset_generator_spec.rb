7a75b416adc58625150a3f685b3fb47445c5b80f
blainelawson <blaine.lawson@gmail.com>
2020-01-15 09:22:50 -0500

    Built additional tests for larger input sample

--- spec/01_dataset_generator_spec.rb
+++ spec/01_dataset_generator_spec.rb
@@ -1,15 +1,22 @@
 require_relative '../config/environment'
 require_relative 'spec_helper'
-require 'pry'
 
 file_path = '../db/input/currency_sample.in.txt'
-file = File.open(file_path)
+file_path2 = '../db/input/test2.in.txt'
 file_data = File.read(file_path).split("\r\n")
+file_data2 = File.read(file_path2).split("\r\n")
 
 
 describe "dataset_generator" do
     it "DatasetGenerator.generate instanciates new dataset objects given input" do
         Dataset.new
         expect(Dataset.all.last).to be_an_instance_of Dataset
-    end            
+    end   
+    
+    it "generates the correct number of datasets per input" do
+        Dataset.all.clear
+        DatasetGenerator.generate(file_data)
+        DatasetGenerator.generate(file_data2)
+        expect(Dataset.all.length).to eq(5)
+    end
 end
--- spec/02_dataset_spec.rb
+++ spec/02_dataset_spec.rb
@@ -1,24 +1,32 @@
 require_relative '../config/environment'
 require_relative 'spec_helper'
-require 'pry'
 
 file_path = '../db/input/currency_sample.in.txt'
-file = File.open(file_path)
-file_data = File.read(file_path).split("\r\n")
+file_path2 = '../db/input/test2.in.txt'
+file_data1 = File.read(file_path).split("\r\n")
+file_data2 = File.read(file_path2).split("\r\n")
 
 
 describe "dataset class" do
 
     before(:each) do
         Dataset.all.clear
-        DatasetGenerator.generate(file_data)
-        @dataset1 = Dataset.all.first
-        @dataset2 = Dataset.all.last
+        DatasetGenerator.generate(file_data1)
+        @dataset1 = Dataset.all[0]
+        @dataset2 = Dataset.all[1]
+
+        DatasetGenerator.generate(file_data2)
+        @dataset3 = Dataset.all[2]
+        @dataset4 = Dataset.all[3]
+        @dataset5 = Dataset.all[4]
     end
     
     it "has appropriate set sizes" do
         expect(@dataset1.prices.length).to eq(2)
-        expect(@dataset2.prices.length).to eq(3)       
+        expect(@dataset2.prices.length).to eq(3)
+        expect(@dataset3.prices.length).to eq(4)
+        expect(@dataset4.prices.length).to eq(2)
+        expect(@dataset5.prices.length).to eq(3)
     end
     
     it "correctly calculates prices in base terms" do
@@ -28,15 +36,30 @@ describe "dataset class" do
         expect(@dataset2.prices[0]).to eq([15,5,1])
         expect(@dataset2.prices[1]).to eq([45,0,0])
         expect(@dataset2.prices[2]).to eq([15,50,0])
+
+        expect(@dataset3.prices[0]).to eq([24,108,8,7])
+        expect(@dataset3.prices[1]).to eq([0,120,20,8])
+        expect(@dataset3.prices[2]).to eq([24,36,0,1])
+        expect(@dataset3.prices[3]).to eq([48,0,0,0])
+
+        expect(@dataset4.prices[0]).to eq([8,2,1])
+        expect(@dataset4.prices[1]) .to eq([16,20,9])
+
+        expect(@dataset5.prices[0]).to eq([0,20,8])
+        expect(@dataset5.prices[1]).to eq([12,8,1])
+        expect(@dataset5.prices[2]).to eq([24,0,0])
     end
 
     it "calculates the max sale in data set correctly" do
         expect(@dataset1.max_sale).to eq(1)
         expect(@dataset2.max_sale).to eq(44)
+        expect(@dataset3.max_sale).to eq(100)
+        expect(@dataset4.max_sale).to eq(34)
+        expect(@dataset5.max_sale).to eq(7)
     end
     
     it "outputs information in correct format" do
-        valid_output = "Dataset 1:\n1\nDataset 2:\n44\n"
+        valid_output = "Dataset 1:\n1\nDataset 2:\n44\nDataset 3:\n100\nDataset 4:\n34\nDataset 5:\n7\n"
 
         expect(Dataset.display).to eq(valid_output)
     end
